a36284a7324bea48c089a0ecb892c81a81961d89
Sean Malan <sean.malan@developersinstitute.co.nz>
2022-09-14 17:50:35 +1200

    started the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import JobList from "./components/JobList";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import CreateJob from "./components/CreateJob";
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Header />
+      <div className="container">
+        
+        <Routes>
+          <Route path="/" element={<JobList />} />
+          <Route path="/active-jobs" element={<JobList />} />
+          <Route path="/add" element={<CreateJob />} />
+        </Routes>
+      </div>
+      <div>
+        
+      </div>
+    </>
   );
-}
+};
 
 export default App;
